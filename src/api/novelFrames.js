51204b56d3cc4df86eb1efd140f58466be2ff285
Thomas Le <34195263+nashkenazy@users.noreply.github.com>
2018-02-25 12:28:50 -0800

    choicesExist is false when not stated in array element

--- src/api/novelFrames.js
+++ src/api/novelFrames.js
@@ -3,8 +3,7 @@ var novelFrames = [
     speaker: "shizuku",
     bg: "bg/rainy_class.png",
     text: "jf",
-    sprite: "sprites/shizuku-laughing.png",
-    choicesExist: "true"
+    sprite: "sprites/shizuku-laughing.png"
   },
   {
     bg: "bg/rainy_class.png",
@@ -19,8 +18,31 @@ var novelFrames = [
   {
     bg: "bg/rainy_class.png",
     text: "jfaaaaa",
+    sprite: "sprites/shizuku-laughing.png",
+    choicesExist: true
+  },
+  {
+    bg: "bg/rainy_class.png",
+    text: "Some text after",
+    sprite: "sprites/shizuku-laughing.png"
+  },
+  {
+    bg: "bg/rainy_class.png",
+    text: "Some text after 2",
+    sprite: "sprites/shizuku-laughing.png",
+    choicesExist: true
+  },
+  {
+    bg: "bg/rainy_class.png",
+    text: "Some text after 3",
     sprite: "sprites/shizuku-laughing.png"
   }
 ];
+
+for (let i = 0; i < novelFrames.length; i++) {
+  if (novelFrames[i].choicesExist !== true) {
+    novelFrames[i].choicesExist = false;
+  }
+}
 
 export default novelFrames;
