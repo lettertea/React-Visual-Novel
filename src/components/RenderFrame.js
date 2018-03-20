3360b8046db3cd22ffc9947309a68c3bd29192f9
Thomas Le <34195263+nashkenazy@users.noreply.github.com>
2018-03-20 01:06:05 -0700

    Cut down code to avoid repeation by using ternary for ReactCSSTransitionGroup props

--- src/components/RenderFrame.js
+++ src/components/RenderFrame.js
@@ -4,49 +4,26 @@ import ReactCSSTransitionGroup from "react-addons-css-transition-group";
 function RenderFrame(props) {
   return (
     <div onClick={props.setNextFrame}>
-      {/* slow css transition for scene change */}
-      {props.sceneChange ? (
-        <div>
-          <ReactCSSTransitionGroup
-            component="div"
-            transitionName="scene"
-            transitionEnterTimeout={2000}
-            transitionLeaveTimeout={1700}
-          >
-            <img key={props.bg} className="bg" src={props.bg} />
-            <img key={props.sprite} className="sprite" src={props.sprite} />
-            {props.text && props.textBoxShown ? (
-              <div className="text-box">
-                {props.speaker ? (
-                  <div className="speaker"> {props.speaker} </div>
-                ) : null}
-                <div className="text"> {props.text} </div>
-              </div>
+      <div>
+        {/* fast css transition for regular, sprite changes. No Text box animation */}
+        <ReactCSSTransitionGroup
+          component="div"
+          transitionName={props.sceneChange ? "scene" : "sprite"}
+          transitionEnterTimeout={props.sceneChange ? 2000 : 400}
+          transitionLeaveTimeout={props.sceneChange ? 1700 : 300}
+        >
+          <img key={props.bg} className="bg" src={props.bg} />
+          <img key={props.sprite} className="sprite" src={props.sprite} />
+        </ReactCSSTransitionGroup>
+        {props.text && props.textBoxShown ? (
+          <div className="text-box">
+            {props.speaker ? (
+              <div className="speaker"> {props.speaker} </div>
             ) : null}
-          </ReactCSSTransitionGroup>
-        </div>
-      ) : (
-        <div>
-          {/* fast css transition for regular, sprite changes. No Text box animation */}
-          <ReactCSSTransitionGroup
-            component="div"
-            transitionName="sprite"
-            transitionEnterTimeout={400}
-            transitionLeaveTimeout={300}
-          >
-            <img key={props.bg} className="bg" src={props.bg} />
-            <img key={props.sprite} className="sprite" src={props.sprite} />
-          </ReactCSSTransitionGroup>
-          {props.text && props.textBoxShown ? (
-            <div className="text-box">
-              {props.speaker ? (
-                <div className="speaker"> {props.speaker} </div>
-              ) : null}
-              <div className="text"> {props.text} </div>
-            </div>
-          ) : null}
-        </div>
-      )}
+            <div className="text"> {props.text} </div>
+          </div>
+        ) : null}
+      </div>
     </div>
   );
 }
