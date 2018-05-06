7d72382586874c4f00a379b5b940b4402dd8e0a9
Thomas Le <34195263+nashkenazy@users.noreply.github.com>
2018-05-06 16:43:27 -0700

    Update sample project

--- src/api/novelFrames.js
+++ src/api/novelFrames.js
@@ -1,5 +1,10 @@
+const b = "Block";
+const bn = require("./sprites/block-neutral.png");
+
 let novelFrames = [
   {
+    speaker: b,
+    sprite: bn,
     text: "So we will keep track of the player's choices."
   },
   {
@@ -34,27 +39,40 @@ let novelFrames = [
     text: "I try picking it, but nothing happens."
   },
   {
-    text: "No key end.",
+    logIndex: true,
+    sceneChange: true,
     jumpTo: "titleScreen"
   },
   // Jumps to below if the user picks up the key
   {
     receiveJumpBecauseStore: "haveKey",
+    sound: require("./sounds/unlock.mp3"),
     text: "I take out the key from my pocket and insert it into the lock."
   },
   {
     text: "The door opens."
   },
   {
-    text: "Key unlocked end.",
+    logIndex: true,
+    sceneChange: true,
     jumpTo: "titleScreen"
   }
 ];
 
 // BG
-for (let i = 0; i < novelFrames.length; i++) {
+for (let i = 0; i < 9; i++) {
   novelFrames[i].bg = require("./bg/microphone.jpeg"); // source https://www.pexels.com/photo/blur-close-up-dark-focus-302655/
 }
+
+novelFrames[9].bg = require("./bg/lockedIn.png");
+
+for (let i = 10; i < 12; i++) {
+  novelFrames[i].bg = require("./bg/microphone.jpeg"); // source https://www.pexels.com/photo/blur-close-up-dark-focus-302655/
+}
+
+novelFrames[12].bg = require("./bg/entrance.jpeg");
+
+// BGM
 
 for (let i = 0; i < novelFrames.length; i++) {
   novelFrames[i].bgm = require("./bgm/take.mp3"); // source http://freemusicarchive.org/music/David_Szesztay/20170730112627440/Throughout_The_City
