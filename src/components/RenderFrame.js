bc91b61645c1c441c48a7f8dfec7e478f38eea76
Thomas Le <34195263+nashkenazy@users.noreply.github.com>
2018-05-20 21:20:20 -0700

    Remove property specific functions

--- src/components/RenderFrame.js
+++ src/components/RenderFrame.js
@@ -2,147 +2,31 @@ import React from "react";
 import ReactCSSTransitionGroup from "react-addons-css-transition-group";
 
 function RenderFrame(props) {
-  function bgTransitionName() {
-    if (props.bgTransition === "scene-change") {
+  function transitionName(key) {
+    if (props[key] === "scene-change") {
       return "scene";
-    } else if (props.bgTransition === "slide-left") {
+    } else if (props[key] === "slide-left") {
       return "slide-left";
-    } else if (props.bgTransition === "slide-right") {
+    } else if (props[key] === "slide-right") {
       return "slide-right";
     } else {
       return "sprite";
     }
   }
-  function bgTransitionEnter() {
-    if (props.bgTransition === "scene-change") {
+  function transitionEnter(key) {
+    if (props[key] === "scene-change") {
       return 2000;
-    } else if (
-      props.bgTransition === "slide-left" ||
-      props.bgTransition === "slide-right"
-    ) {
+    } else if (props[key] === "slide-left" || props[key] === "slide-right") {
       return 1000;
     } else {
       return 250;
     }
   }
 
-  function bgTransitionLeave() {
-    if (props.bgTransition === "scene-change") {
+  function transitionLeave(key) {
+    if (props[key] === "scene-change") {
       return 1700;
-    } else if (
-      props.bgTransition === "slide-left" ||
-      props.bgTransition === "slide-right"
-    ) {
-      return 1000;
-    } else {
-      return 250;
-    }
-  }
-
-  function spriteTransitionName() {
-    if (props.spriteTransition === "scene-change") {
-      return "scene";
-    } else if (props.spriteTransition === "slide-left") {
-      return "slide-left";
-    } else if (props.spriteTransition === "slide-right") {
-      return "slide-right";
-    } else {
-      return "sprite";
-    }
-  }
-  function spriteTransitionEnter() {
-    if (props.spriteTransition === "scene-change") {
-      return 2000;
-    } else if (
-      props.spriteTransition === "slide-left" ||
-      props.spriteTransition === "slide-right"
-    ) {
-      return 1000;
-    } else {
-      return 250;
-    }
-  }
-
-  function spriteTransitionLeave() {
-    if (props.spriteTransition === "scene-change") {
-      return 1700;
-    } else if (
-      props.spriteTransition === "slide-left" ||
-      props.spriteTransition === "slide-right"
-    ) {
-      return 1000;
-    } else {
-      return 250;
-    }
-  }
-
-  function spriteLeftTransitionName() {
-    if (props.spriteLeftTransition === "scene-change") {
-      return "scene";
-    } else if (props.spriteLeftTransition === "slide-left") {
-      return "slide-left";
-    } else if (props.spriteLeftTransition === "slide-right") {
-      return "slide-right";
-    } else {
-      return "sprite";
-    }
-  }
-  function spriteLeftTransitionEnter() {
-    if (props.spriteLeftTransition === "scene-change") {
-      return 2000;
-    } else if (
-      props.spriteLeftTransition === "slide-left" ||
-      props.spriteLeftTransition === "slide-right"
-    ) {
-      return 1000;
-    } else {
-      return 250;
-    }
-  }
-
-  function spriteLeftTransitionLeave() {
-    if (props.spriteLeftTransition === "scene-change") {
-      return 1700;
-    } else if (
-      props.spriteLeftTransition === "slide-left" ||
-      props.spriteLeftTransition === "slide-right"
-    ) {
-      return 1000;
-    } else {
-      return 250;
-    }
-  }
-  function spriteRightTransitionName() {
-    if (props.spriteRightTransition === "scene-change") {
-      return "scene";
-    } else if (props.spriteRightTransition === "slide-left") {
-      return "slide-left";
-    } else if (props.spriteRightTransition === "slide-right") {
-      return "slide-right";
-    } else {
-      return "sprite";
-    }
-  }
-  function spriteRightTransitionEnter() {
-    if (props.spriteRightTransition === "scene-change") {
-      return 2000;
-    } else if (
-      props.spriteRightTransition === "slide-left" ||
-      props.spriteRightTransition === "slide-right"
-    ) {
-      return 1000;
-    } else {
-      return 250;
-    }
-  }
-
-  function spriteRightTransitionLeave() {
-    if (props.spriteRightTransition === "scene-change") {
-      return 1700;
-    } else if (
-      props.spriteRightTransition === "slide-left" ||
-      props.spriteRightTransition === "slide-right"
-    ) {
+    } else if (props[key] === "slide-left" || props[key] === "slide-right") {
       return 1000;
     } else {
       return 250;
@@ -155,16 +39,16 @@ function RenderFrame(props) {
         <ReactCSSTransitionGroup
           component="div"
           className="sprite-center"
-          transitionName={bgTransitionName()}
-          transitionEnterTimeout={bgTransitionEnter()}
-          transitionLeaveTimeout={bgTransitionLeave()}
+          transitionName={transitionName("bgTransition")}
+          transitionEnterTimeout={transitionEnter("bgTransition")}
+          transitionLeaveTimeout={transitionLeave("bgTransition")}
         >
           <img key={props.bg} className="bg" src={props.bg} />
           <ReactCSSTransitionGroup
             className="sprite-center"
-            transitionName={spriteTransitionName()}
-            transitionEnterTimeout={spriteTransitionEnter()}
-            transitionLeaveTimeout={spriteTransitionLeave()}
+            transitionName={transitionName("spriteTransition")}
+            transitionEnterTimeout={transitionEnter("spriteTransition")}
+            transitionLeaveTimeout={transitionLeave("spriteTransition")}
           >
             <img
               key={props.sprite}
@@ -174,9 +58,9 @@ function RenderFrame(props) {
           </ReactCSSTransitionGroup>
           <ReactCSSTransitionGroup
             className="sprite-center"
-            transitionName={spriteLeftTransitionName()}
-            transitionEnterTimeout={spriteLeftTransitionEnter()}
-            transitionLeaveTimeout={spriteLeftTransitionLeave()}
+            transitionName={transitionName("spriteLeftTransition")}
+            transitionEnterTimeout={transitionEnter("spriteLeftTransition")}
+            transitionLeaveTimeout={transitionLeave("spriteLeftTransition")}
           >
             <img
               key={props.spriteLeft + "left"}
@@ -186,9 +70,9 @@ function RenderFrame(props) {
           </ReactCSSTransitionGroup>
           <ReactCSSTransitionGroup
             className="sprite-center"
-            transitionName={spriteRightTransitionName()}
-            transitionEnterTimeout={spriteRightTransitionEnter()}
-            transitionLeaveTimeout={spriteRightTransitionLeave()}
+            transitionName={transitionName("spriteRightTransition")}
+            transitionEnterTimeout={transitionEnter("spriteRightTransition")}
+            transitionLeaveTimeout={transitionLeave("spriteRightTransition")}
           >
             <img
               key={props.spriteRight + "right"}
