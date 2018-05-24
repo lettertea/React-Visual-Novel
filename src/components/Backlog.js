4e0da29653381c293d775f59bf66b02035a1f119
Thomas Le <34195263+nashkenazy@users.noreply.github.com>
2018-05-24 16:29:06 -0700

    Fix jump method when past one choice

--- src/components/Backlog.js
+++ src/components/Backlog.js
@@ -1,6 +1,5 @@
 import React from "react";
 import novelFrames from "../story/novelFrames";
-import Choices from "../story/Choices";
 
 function Backlog(props) {
   function handleJump(i, choicesIndex) {
@@ -13,18 +12,21 @@ function Backlog(props) {
   let choicesIndex = 0;
   let pastFirstChoice = false;
   for (let i = 0; i <= props.index; i++) {
+    if (novelFrames[i].choicesExist && pastFirstChoice) {
+      choicesIndex++;
+    }
+    if (novelFrames[i].choicesExist) {
+      pastFirstChoice = true;
+    }
     textHistory.unshift(
       <div className="backlog" key={i}>
         <div className="backlog-jump-container" onClick={() => handleJump(i, choicesIndex)}>
-          <span className="backlog-jump-text">Jump</span>
+          <span className="backlog-jump-text">{i === props.index ? null : "Jump"}</span>
         </div>
         <div className="backlog-speaker">{novelFrames[i].speaker}</div>
         {novelFrames[i].text}
       </div>
     );
-    if (novelFrames[i].choicesExist && pastFirstChoice) {
-      choicesIndex++;
-    }
   }
 
   return <div className="overlay backlog-overlay">{textHistory}</div>;
