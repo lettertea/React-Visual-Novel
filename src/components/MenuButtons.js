bb6932f0eaa97d6cdf9828c9818d2283861aae42
Thomas Le <34195263+nashkenazy@users.noreply.github.com>
2018-06-01 14:04:14 -0700

    Add key action events

--- src/components/MenuButtons.js
+++ src/components/MenuButtons.js
@@ -1,18 +1,24 @@
 import React from "react";
+import KeyHandler, { KEYDOWN, KEYUP } from "react-key-handler";
 
 function MenuButtons(props) {
+  function handleToggles(event, value, toggle) {
+    return <KeyHandler keyEventName={event} keyValue={value} onKeyHandle={toggle} />;
+  }
+
   return (
     <div className="container menu-buttons-container">
       <div className="menu-buttons">
-        <button onClick={props.toggleTextBox}>{props.textBoxShown ? "Hide Text Box" : "Show Text Box"}</button>
-        <button onClick={props.toggleBacklog}>{props.backlogShown ? "Hide Backlog" : "Backlog"}</button>
+        {handleToggles(KEYDOWN, " ", props.toggleTextBox)}
+        {handleToggles(KEYDOWN, "Control", props.setNextFrame)}
+        {handleToggles(KEYUP, "Enter", props.setNextFrame)}
         {props.isSkipping ? (
           <button onClick={props.stopSkip}>Stop</button>
         ) : (
           <button onClick={props.startSkip}>Skip</button>
         )}
 
-        <button onClick={props.toggleSaveMenu}>{props.saveMenuShown ? "Hide Saves" : "Save"}</button>
+        <button onWheel={props.toggleBacklog}>{props.saveMenuShown ? "Hide Saves" : "Save"}</button>
         <button onClick={props.toggleLoadMenu}>{props.loadMenuShown ? "Hide Loads" : "Load"}</button>
         <button onClick={props.toggleConfigMenu}>{props.configMenuShown ? "Hide Config" : "Config"}</button>
         <button onClick={props.toggleFullscreen} style={{ float: "right" }}>
